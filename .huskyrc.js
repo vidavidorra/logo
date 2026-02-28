9ade9ed2b792ea098bd2e15e075888e571d70c9e
Jeroen de Bruijn <vv9239@gmail.com>
2026-02-28 11:14:23 +0100

    feat!: remove all tools except the vidavidorra-logo
    BREAKING CHANGE: remove all tools except the vidavidorra-logo. The future purpose of this repository is the logo.
    
    This makes the repository at an intermediate state as the vidavidorra-logo tool is now broken. The next step is to refactor the logo as standalone, making that the sole purpose of this repository.

--- .huskyrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': 'lint-staged && npm run build',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  },
-};
--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '*.{ts,tsx,js,jsx,json}': ['npm run lint-es:file:fix'],
-  '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
-    'npm run format:file',
-  ],
-};
